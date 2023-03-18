aeeedb692c4e1d65e6731deea23fb34d47b32131
MH <amjadmhasnain@gmail.com>
2023-03-18 19:37:57 +0500

    .

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,14 @@ $(".icon").click(function(){
       $("ul").slideToggle();
 })
 
+$(".lb").click(function(){
+      $("form-box").slideToggle();
+})
 
+
+
+
+const lb  = document.querySelector('.lb')
+lb.addEventListener('click', function(){
+      form-box.classlist.add('active-popup');
+});
